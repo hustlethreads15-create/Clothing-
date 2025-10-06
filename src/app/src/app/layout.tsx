2210e78c5a16f8d2fa7b543abcf789b0cc0c6895
hustlethreads15-create <hustle.threads15@gmail.com>
2025-10-06 10:57:11 +0200

    Create layout.tsx

---
+++ src/app/src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "ChicWear",
+  description: "Clothing store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900">{children}</body>
+    </html>
+  );
+    }
